fb257e1e2456857a804337043038caaa95958e8c
Omar Nassar <omar.portero.nassar1@gmail.com>
2025-06-22 09:13:22 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/meal-mat-ai-site' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/meal-mate-ai-site' : '',
   images: {
     unoptimized: true,
   },
